19df0d5d19a573ae2a3583f974485db0e4742122
Thanishma71 <238w1a1271@vrsec.ac.in>
2026-04-16 16:21:41 +0530

    day 2 admin, add product

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 app.use(cors());
 app.use(express.json());
@@ -25,4 +26,5 @@ app.get("/", (req, res) => {
   res.json({ message: "server is running" });
 });
 app.use("/api/auth", authRoutes);
+app.use("/api/product", productRoutes);
 app.listen(5000, () => console.log("server runns on port 5000"));
